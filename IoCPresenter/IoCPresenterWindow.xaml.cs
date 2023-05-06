f83c8659da982d8b96c484256b9988b2d7ad1a40
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-05-06 22:03:25 +0500

    UPD

--- IoCPresenter/IoCPresenterWindow.xaml.cs
+++ IoCPresenter/IoCPresenterWindow.xaml.cs
@@ -8,28 +8,48 @@ namespace IoCPresenter;
 
 public partial class IoCPresenterWindow : Window
 {
+    private IServiceCollection _serviceCollection;
+    private List<ServiceDescriptorViewModel> _services;
+
     public IoCPresenterWindow(IServiceCollection serviceCollection)
     {
         InitializeComponent();
-        LoadServicesIntoListView(serviceCollection);
+        _serviceCollection = serviceCollection;
+        LoadServicesIntoListView(_serviceCollection);
     }
 
-    private void LoadServicesIntoListView(IServiceCollection serviceCollection)
+    private void LoadServicesIntoListView(IServiceCollection serviceCollection, bool excludeSystemServices = false)
     {
-        var services = new List<ServiceDescriptorViewModel>();
+        _services = new List<ServiceDescriptorViewModel>();
 
         foreach (var descriptor in serviceCollection)
         {
-            services.Add(new ServiceDescriptorViewModel
+            var viewModel = new ServiceDescriptorViewModel
             {
                 Name = descriptor.ServiceType.Name,
                 Type = descriptor.ServiceType.FullName,
                 Lifetime = descriptor.Lifetime.ToString(),
                 ImplementationType = descriptor.ImplementationType?.Name ?? "N/A",
                 Assembly = descriptor.ServiceType.Assembly.FullName
-            });
+            };
+
+            if (!excludeSystemServices || !viewModel.Assembly.StartsWith("Microsoft.Extensions"))
+            {
+                _services.Add(viewModel);
+            }
         }
 
-        ServicesListView.ItemsSource = services;
+        ServicesListView.ItemsSource = _services;
+    }
+
+    private void ExcludeSystemServicesCheckBox_Checked(object sender, RoutedEventArgs e)
+    {
+        LoadServicesIntoListView(_serviceCollection, true);
+    }
+
+    private void ExcludeSystemServicesCheckBox_Unchecked(object sender, RoutedEventArgs e)
+    {
+        LoadServicesIntoListView(_serviceCollection, false);
     }
 }
+
